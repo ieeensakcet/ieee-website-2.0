c4fd144178c5a3a5750a9942f5011f1e880c7b9a
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-03-24 13:38:51 +0530

    file cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../components/footer/footer'
+import Footer from '../components/footer/Footer'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
