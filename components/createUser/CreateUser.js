8651f26b229e9d6e6ec5f4c02e2d875bf2342cbc
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-04-03 15:07:58 +0530

    created create user component

--- components/createUser/CreateUser.js
+++ components/createUser/CreateUser.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import { auth } from "../../config/firebaseConfig";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import useProvideAuth from "../../utils/auth";
 
 //styles
-import styles from "./SignIn.module.css";
+import styles from "./CreateUser.module.css";
 import { Button, TextField } from "@mui/material";
-import useProvideAuth from "../../utils/auth";
 
 export default function CreateUser() {
   // const dispatch = useDispatch()
@@ -16,34 +14,26 @@ export default function CreateUser() {
     password: "",
   });
 
-  const createUser = (e) => {
+  const creates = (e) => {
     e.preventDefault();
     if(!userInfo.email || !userInfo.password){
       alert("enter email and password")
       return;
     }
     auth.signup(userInfo.email, userInfo.password)
-    // signInWithEmailAndPassword(auth, userInfo.email, userInfo.password)
-    //   .then((userAuth) => {
-    //     // dispatch(login({
-    //     //     email: userAuth.user.email,
-    //     //     uid: userAuth.user.uid,
-    //     // }))
-    //     // history.replace("/");
-    //     alert("Successfully Logged In");
-    //   })
-    //   .catch((error) => alert(error.message));
   };
 
   const onInputChange = (e) => {
     setUserInfo({ ...userInfo, [e.target.type]: e.target.value });
   };
+
+  const handleCloseClick = (e) => {
+    e.preventDefault();
+    onClose();
+  };
   return (
     <div>
       <h1 className={styles.title}>Create User<span className={styles.span}>Account</span></h1>
-      <h5 className={styles.subTitle}>
-        Not a Member Yet? <a className={styles.span}>Join Us</a>
-      </h5>
       <div className={styles.form}>
           <TextField
             id="filled-email-input"
@@ -63,7 +53,7 @@ export default function CreateUser() {
             value={userInfo.password}
             onChange={onInputChange}
           />
-          <Button variant="contained" onClick={createUser()}>Create User</Button>
+          <Button variant="contained" onClick={creates}>Create User</Button>
 
           {/* <button className="button">Login</button> */}
       </div>
