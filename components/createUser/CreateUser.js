e96373f48c9eca097cca3103fd7027bb87eebab3
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-04-04 15:51:48 +0530

    [fixed] model created for create user

--- components/createUser/CreateUser.js
+++ components/createUser/CreateUser.js
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import useProvideAuth from "../../utils/auth";
 
 //styles
 import styles from "./CreateUser.module.css";
 import { Button, TextField } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function CreateUser() {
+export default function CreateUser({ showModal, setShowModal }) {
+  const modalRef = useRef();
   // const dispatch = useDispatch()
-  const auth = useProvideAuth()
+  const auth = useProvideAuth();
 
   const [userInfo, setUserInfo] = useState({
     email: "",
@@ -16,47 +18,91 @@ export default function CreateUser() {
 
   const creates = (e) => {
     e.preventDefault();
-    if(!userInfo.email || !userInfo.password){
-      alert("enter email and password")
+    if (!userInfo.email || !userInfo.password) {
+      alert("enter email and password");
       return;
     }
-    auth.signup(userInfo.email, userInfo.password)
+    auth.signup(userInfo.email, userInfo.password);
   };
 
   const onInputChange = (e) => {
     setUserInfo({ ...userInfo, [e.target.type]: e.target.value });
   };
-
-  const handleCloseClick = (e) => {
-    e.preventDefault();
-    onClose();
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
   };
-  return (
-    <div>
-      <h1 className={styles.title}>Create User<span className={styles.span}>Account</span></h1>
-      <div className={styles.form}>
-          <TextField
-            id="filled-email-input"
-            label="Email"
-            type="email"
-            autoComplete="off"
-            variant="filled"
-            value={userInfo.email}
-            onChange={onInputChange}
-          />
-          <TextField
-            id="filled-password-input"
-            label="Password"
-            type="password"
-            autoComplete="off"
-            variant="filled"
-            value={userInfo.password}
-            onChange={onInputChange}
-          />
-          <Button variant="contained" onClick={creates}>Create User</Button>
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === "Escape" && showModal) {
+        setShowModal(false);
+        console.log("I pressed");
+      }
+    },
+    [setShowModal, showModal]
+  );
 
-          {/* <button className="button">Login</button> */}
-      </div>
-    </div>
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
+
+  return (
+    <>
+      {showModal ? (
+        <div
+          className={styles.modal__background}
+          onClick={closeModal}
+          ref={modalRef}
+        >
+          <div className={styles.modal__wrapper} showModal={showModal}>
+            <div>
+              <Button
+                onClick={() => setShowModal((prev) => !prev)}
+                variant="contained"
+                endIcon={<CloseIcon />}
+                sx={{
+                  position: "absolute",
+                  right: 0,
+                  top: 0,
+                  backgroundColor: "#db2b39",
+                }}
+              >
+                Close
+              </Button>
+              <h1 className={styles.title}>
+                Create User<span className={styles.span}>Account</span>
+              </h1>
+              <div className={styles.form}>
+                <TextField
+                  id="filled-email-input"
+                  label="Email"
+                  type="email"
+                  autoComplete="off"
+                  variant="filled"
+                  value={userInfo.email}
+                  onChange={onInputChange}
+                />
+                <TextField
+                  id="filled-password-input"
+                  label="Password"
+                  type="password"
+                  autoComplete="off"
+                  variant="filled"
+                  value={userInfo.password}
+                  onChange={onInputChange}
+                />
+                <Button variant="contained" onClick={creates}>
+                  Create User
+                </Button>
+
+                {/* <button className="button">Login</button> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 }
