e33649f31071384f3775e54609e35cc6c6703be1
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-04-18 15:19:56 +0530

    combiing functions

--- functions/index.js
+++ functions/index.js
@@ -12,11 +12,37 @@ exports.register = functions
         email: data.email,
         emailVerified: false,
         password: data.password,
-        displayName: "John Dor",
+        displayName: data.displayName,
         photoURL: "http://www.example.com/12345678/photo.png",
         disabled: false,
       })
       .then((userRecord) => {
+        const firestore = admin.firestore();
+        const userId = userRecord.uid;
+        const userRef = firestore.collection("users").doc(userId);
+        const role = data.role;
+        userRef
+          .set({
+            email: userRecord.email,
+            emailVerified: userRecord.emailVerified,
+            displayName: userRecord.displayName,
+            customClaims: data.role,
+            membershipNumber: data.membershipNumber,
+            uid: userRecord.uid,
+          })
+          .then((userId) => {
+            // console.log(userId);
+            admin.auth().setCustomUserClaims(userId, {
+              role: true,
+            })
+          })
+          .then(() => {
+            //Interesting to note: we need to re-fetch the userRecord, as the user variable **does not** hold the claim
+            return admin.auth().getUser(userId);
+          })
+          .catch((error) => {
+            console.log("Error added new user:", error);
+          });
         // See the UserRecord reference doc for the contents of userRecord.
         console.log("Successfully created new user:", userRecord.uid);
       })
@@ -25,29 +51,32 @@ exports.register = functions
       });
   });
 
-  
-exports.userSaver = functions.auth.user().onCreate(async user => {
-  console.log(user)
-  const firestore = admin.firestore()
-  const userId = user.uid
-  const userRef = firestore.collection("users").doc(userId)
-  await userRef.set({
-    email: user.email,
-    emailVerified: user.emailVerified,
-    displayName: user.displayName,
-    customClaims: {},
-    uid: user.uid,
-  }).then(
-    // See the UserRecord reference doc for the contents of userRecord.
-    console.log("Successfully added new user")
-  )
-  .catch((error) => {
-    console.log("Error added new user:", error);
-  });
-})
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// exports.userSaver = functions.auth.user().onCreate(async (user) => {
+//   const userId = user.uid;
+//   return admin.auth().setCustomUserClaims(userId, {
+//     isAdmin: true,
+//   })
+//   // await userRef
+//   //   .set({
+//   //     email: user.email,
+//   //     emailVerified: user.emailVerified,
+//   //     displayName: user.displayName,
+//   //     customClaims: {},
+//   //     uid: user.uid,
+//   //   })
+//     .then(() => {
+//       //Interesting to note: we need to re-fetch the userRecord, as the user variable **does not** hold the claim
+//       return admin.auth().getUser(userId);
+//     })
+//     .then((userRecord) => {
+//       console.log(userId);
+//       console.log(userRecord.customClaims.isAdmin);
+//       return null;
+//     })
+//     .catch((error) => {
+//       console.log("Error:", error);
+//     });
+// });
 
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", { structuredData: true });
