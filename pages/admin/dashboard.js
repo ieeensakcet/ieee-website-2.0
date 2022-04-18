59881b5e66f76d5c4f3efb24b3ec7a84fe6042cf
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-04-18 15:18:57 +0530

    displaying data from database

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,36 +1,68 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../../components/footer/Footer";
 import styles from "../../styles/Dashboard.module.css";
 import { DataGrid } from "@mui/x-data-grid";
 import {
+  Box,
   Button,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Modal,
   Typography,
 } from "@mui/material";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import DeleteIcon from "@mui/icons-material/Delete";
 import LogoutIcon from "@mui/icons-material/Logout";
-import CreateUser from "../../components/createUser/CreateUser";
+import CloseIcon from "@mui/icons-material/Close";
+import CreateUserForm from "../../components/createUserForm/CreateUserForm";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebaseConfig";
 
+// import { getUsers, users } from "../../helpers/userDB";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 8,
+  textAlign: 'center'
+};
 export default function Dashboard() {
-  const [showModal, setShowModal] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(collection(db, "users"));
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // const [showModal, setShowModal] = useState(false);
   const [selectedRows, setSelectedRows] = useState([]);
   console.log(selectedRows);
 
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
 
   const openModal = () => {
     setShowModal((prev) => !prev);
@@ -38,80 +70,34 @@ export default function Dashboard() {
 
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "displayName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
     // {
-    //   field: "Full Name",
-    //   headerName: "First name",
-    //   width: 150,
-    //   editable: true,
+    //   field: "fullName",
+    //   headerName: "Full name",
+    //   description: "This column has a value getter and is not sortable.",
+    //   // sortable: false,
+    //   width: 250,
+    //   valueGetter: (params) =>
+    //     `${params.row.firstName || ""} ${params.row.lastName || ""}`,
     // },
     {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      // sortable: false,
+      field: "email",
+      headerName: "Email ID",
       width: 250,
-      valueGetter: (params) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+      editable: true,
     },
     {
-      field: "role",
+      field: "customClaims",
       headerName: "Site Role",
       width: 250,
       editable: true,
     },
   ];
-
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35, role: "admin" },
-    {
-      id: 2,
-      lastName: "Lannister",
-      firstName: "Cersei",
-      age: 42,
-      role: "web master",
-    },
-    {
-      id: 3,
-      lastName: "Lannister",
-      firstName: "Jaime",
-      age: 45,
-      role: "member",
-    },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 10, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 11, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 12, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 13, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 14, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 15, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 16, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 17, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 18, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 19, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 20, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 21, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 22, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 23, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 24, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 25, lastName: "Roxie", firstName: "Harvey", age: 65 },
-    { id: 26, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  ];
-
-  console.log(showModal);
-  const user = (e) => {
-    // e.preventDefault();
-    // if(!userInfo.email || !userInfo.password){
-    //   alert("enter email and password")
-    //   return;
-    // }
-    console.log("user");
-    // auth.signup(userInfo.email, userInfo.password)
-  };
 
   return (
     <div className={styles.container}>
@@ -139,14 +125,6 @@ export default function Dashboard() {
             </Button>
           </div>
         </div>
-        {/* <Button variant="contained" onClick={() => setShowModal(true)}>
-          Add User
-        </Button>
-        {showModal ? (
-          <CreateUser onClose={() => setShowModal(false)} show={showModal} />
-        ) : (
-          ""
-        )} */}
         <div className={styles.dashboard__container}>
           <div className={styles.dashboard__container__header}>
             <div className={styles.dashboard__container__header__content}>
@@ -160,7 +138,11 @@ export default function Dashboard() {
               <div
                 className={styles.dashboard__container__header__buttons__boxRed}
               >
-                <Button sx={{ color: "#fff" }} endIcon={<DeleteIcon />} onClick={handleClickOpen}>
+                <Button
+                  sx={{ color: "#fff" }}
+                  endIcon={<DeleteIcon />}
+                  onClick={handleClickOpen}
+                >
                   Delete User
                 </Button>
                 <Dialog
@@ -174,12 +156,15 @@ export default function Dashboard() {
                   </DialogTitle>
                   <DialogContent>
                     <DialogContentText id="alert-dialog-description">
-                      Do you really want to delete the user? This cannot be undone.
+                      Do you really want to delete the user? This cannot be
+                      undone.
                     </DialogContentText>
                   </DialogContent>
                   <DialogActions>
-                    <Button onClick={handleClose} autoFocus>No</Button>
-                    <Button onClick={handleClose} sx={{color: 'red'}}>
+                    <Button onClick={handleClose} autoFocus>
+                      No
+                    </Button>
+                    <Button onClick={handleClose} sx={{ color: "red" }}>
                       Yes, Delete User
                     </Button>
                   </DialogActions>
@@ -191,17 +176,43 @@ export default function Dashboard() {
                 <Button
                   sx={{ color: "#fff" }}
                   endIcon={<AddBoxIcon />}
-                  onClick={openModal}
+                  // onClick={openModal}
+                  onClick={handleOpen}
                 >
                   Add User
                 </Button>
-                <CreateUser showModal={showModal} setShowModal={setShowModal} />
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={style}>
+                    <Button
+                      onClick={() => setOpen((prev) => !prev)}
+                      variant="contained"
+                      endIcon={<CloseIcon />}
+                      sx={{
+                        position: "absolute",
+                        right: 0,
+                        top: 0,
+                        backgroundColor: "#db2b39",
+                      }}
+                    >
+                      Close
+                    </Button>
+                    <h1 className={styles.title__modal}>
+                      Create User<span className={styles.span}>Account</span>
+                    </h1>
+                    <CreateUserForm />
+                  </Box>
+                </Modal>
               </div>
             </div>
           </div>
           <div style={{ height: 400, width: "100%" }}>
             <DataGrid
-              rows={rows}
+              rows={users}
               columns={columns}
               pageSize={25}
               rowsPerPageOptions={[25]}
@@ -209,10 +220,10 @@ export default function Dashboard() {
               disableSelectionOnClick
               onSelectionModelChange={(ids) => {
                 const selectedIDs = new Set(ids);
-                const selectedRowData = rows.filter((row) =>
-                  selectedIDs.has(row.id)
+                const selectedRowData = users.filter((user) =>
+                  selectedIDs.has(user.id)
                 );
-                console.log(selectedRowData);
+                setSelectedRows(selectedRowData)
               }}
               sx={{ border: "none" }}
             />
