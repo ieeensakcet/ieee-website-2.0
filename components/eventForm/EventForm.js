ce0c7a29aec588bd228b4f00aefa0ba5cf0578b0
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-05-12 16:54:39 +0530

    handling image upload and adding to firestore

--- components/eventForm/EventForm.js
+++ components/eventForm/EventForm.js
@@ -1,4 +1,14 @@
-import { Button, Grid, MenuItem, TextField } from "@mui/material";
+import {
+  Button,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  MenuItem,
+  Paper,
+  TextField,
+  ListItemIcon,
+} from "@mui/material";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { addEvent } from "../../helpers/eventsDB";
@@ -6,8 +16,48 @@ import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import styles from "./EventForm.module.css";
+import Dropzone from "react-dropzone";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import { storage } from "../../config/firebaseConfig";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import LinearProgressWithLabel from "../progressBar/ProgressBar";
 
 function EventForm() {
+  const [urls, setUrls] = useState([]);
+  const [progress, setProgress] = useState(0);
+
+  const uploadFiles = (data) => {
+    const promises = [];
+    data.images.map((file) => {
+      console.log("loop");
+
+      const sotrageRef = ref(storage, `files/${file.name}`);
+
+      const uploadTask = uploadBytesResumable(sotrageRef, file);
+      promises.push(uploadTask);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const prog = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(prog);
+        },
+        (error) => console.log(error),
+        async () => {
+          await getDownloadURL(uploadTask.snapshot.ref).then((downloadURLs) => {
+            setUrls((prevState) => [...prevState, downloadURLs]);
+            console.log("File available at", downloadURLs);
+          });
+        }
+      );
+    });
+    Promise.all(promises)
+      .then(() => alert("All images uploaded"))
+      .then((err) => console.log(err));
+  };
+
   const defaultValues = {
     title: "",
     eventType: "",
@@ -15,13 +65,14 @@ function EventForm() {
     venue: "",
     description: "",
     details: "",
-    images: "",
+    images: [],
     link: "",
     scheduleType: "",
   };
   const { formState, getValues, watch, control, register, handleSubmit } =
     useForm({
       defaultValues,
+      mode: "onBlur",
     });
   const { errors } = formState;
 
@@ -30,6 +81,7 @@ function EventForm() {
 
   const createEvent = async (data) => {
     console.log("running");
+    await uploadFiles(data);
     await addEvent({
       title: data.title,
       eventType: data.eventType,
@@ -37,7 +89,7 @@ function EventForm() {
       venue: data.venue,
       description: data.description,
       details: data.details,
-      images: data.images,
+      images: urls,
       link: data.link,
       scheduleType: data.scheduleType,
     });
@@ -45,7 +97,7 @@ function EventForm() {
 
   const onSubmit = (data) => {
     setData(data);
-    // createEvent(data);
+    createEvent(data);
   };
 
   return (
@@ -190,38 +242,70 @@ function EventForm() {
               <Controller
                 name="date"
                 control={control}
-                render={({
-                  field: { onChange, value },
-                }) => (
+                render={({ field: { onChange, value } }) => (
                   <DateTimePicker
                     label="Date and Time"
                     value={value}
-                    onChange={
-                      (value) => 
-                      onChange(value)
-                    }
+                    onChange={(value) => onChange(value)}
                     renderInput={(params) => (
                       // console.log(invalid),
-                      (
-                        <TextField
-                          variant="filled"
-                          label="Date"
-                          inputProps={register("date", {
-                            required: "Please enter date",
-                          })}
-                          error={errors.date}
-                          helperText={errors.date?.message}
-                          {...params}
-                          fullWidth
-                        />
-                      )
+                      <TextField
+                        variant="filled"
+                        label="Date"
+                        inputProps={register("date", {
+                          required: "Please enter date",
+                        })}
+                        error={errors.date}
+                        helperText={errors.date?.message}
+                        {...params}
+                        fullWidth
+                      />
                     )}
                   />
                 )}
               />
             </LocalizationProvider>
           </Grid>
+          <Grid item xs={12}>
+            <Controller
+              control={control}
+              name="images"
+              defaultValue={[]}
+              render={({ field: { onChange, value, onBlur } }) => (
+                <>
+                  <Dropzone onDrop={onChange}>
+                    {({ getRootProps, getInputProps }) => (
+                      <Paper
+                        variant="outlined"
+                        className={styles.root}
+                        {...getRootProps()}
+                      >
+                        <CloudUploadIcon />
+                        <input
+                          {...getInputProps()}
+                          name="images"
+                          onBlur={onBlur}
+                        />
+                        <p>Drag n drop files here, or click to select files</p>
+                      </Paper>
+                    )}
+                  </Dropzone>
+                  <List>
+                    {value?.map((f, index) => (
+                      <ListItem key={index}>
+                        <ListItemIcon>
+                          <InsertDriveFileIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={f.name} secondary={f.size} />
+                      </ListItem>
+                    ))}
+                  </List>
+                </>
+              )}
+            />
+          </Grid>
         </Grid>
+        <LinearProgressWithLabel value={progress} />
         <Button variant="contained" type="submit">
           Create Event
         </Button>
