ea91db457a2407d5fa9fb9c3262b8ba2f19c950f
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-05-16 18:06:46 +0530

    adding firestore update

--- helpers/eventsDB.js
+++ helpers/eventsDB.js
@@ -1,36 +1,50 @@
-import { collection, addDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../config/firebaseConfig";
 
 // collection ref
 const eventsCollectionRef = collection(db, "events");
 
 const addEvent = async (data) => {
-  await addDoc(
-    eventsCollectionRef,
-    {
-      title: data.title,
-      eventType: data.eventType,
-      date: data.date,
-      venue: data.venue,
-      description: data.description,
-      details: data.details,
-      images: data.images,
-      reviews: data.reviews,
-      link: data.link,
-      scheduleType: data.scheduleType,
-    }
-  );
+  await addDoc(eventsCollectionRef, {
+    title: data.title,
+    eventType: data.eventType,
+    date: data.date,
+    venue: data.venue,
+    description: data.description,
+    details: data.details,
+    images: data.images,
+    link: data.link,
+    scheduleType: data.scheduleType,
+  });
 };
 
-const updateEvent = async (id, age) => {
-  const eventDoc = doc(db, "events", id);
-  const newFields = { age: age + 1 };
-  await updateDoc(eventDoc, newFields);
+const updateEvent = async (id, data) => {
+  console.log(id)
+  console.log(data)
+  const eventDocRef = doc(db, "events", id);
+  const updateFields = {
+    title: data.title,
+    eventType: data.eventType,
+    date: data.date,
+    venue: data.venue,
+    description: data.description,
+    details: data.details,
+    // images: data.images,
+    link: data.link,
+    scheduleType: data.scheduleType,
+  };
+  await updateDoc(eventDocRef, updateFields);
 };
 
 const deleteEvent = async (id) => {
-  const eventDoc = doc(db, "events", id);
-  await deleteDoc(eventDoc);
+  const eventDocRef = doc(db, "events", id);
+  await deleteDoc(eventDocRef);
 };
 
-export {addEvent, updateEvent, deleteEvent}
+export { addEvent, updateEvent, deleteEvent };
