372059ac70b9e100dabd63b70893c62629d45022
mkhalidxx <khalid.26227@gmail.com>
2022-05-31 17:29:17 +0530

    translation(1)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /* @type {import('next').NextConfig} */
 // const nextConfig = {
 // }
+
+const { i18n } = require("./next-i18next.config.js");
+
 module.exports = {
   images: {
-    domains: ['images.unsplash.com','ik.imagekit.io'],
+    domains: ["images.unsplash.com", "ik.imagekit.io"],
   },
   reactStrictMode: true,
-}
+  i18n,
+};
+
+// const nextTranslate = require("next-translate");
+
+// module.exports = nextTranslate({
+//   reactStrictMode: true,
+// });
+
 // module.exports = nextConfig
