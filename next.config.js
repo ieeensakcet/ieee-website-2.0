fa104a368f793108f33786de5ddf7796657121ea
mkhalidxx <khalid.26227@gmail.com>
2022-06-14 01:21:19 +0530

    Resolved merge conflicts

--- next.config.js
+++ next.config.js
@@ -6,16 +6,7 @@ const { i18n } = require("./next-i18next.config.js");
 
 module.exports = {
   images: {
-<<<<<<< HEAD
-    domains: [
-      "images.unsplash.com",
-      "ik.imagekit.io",
-      "source.unsplash.com",
-      "firebasestorage.googleapis.com",
-    ],
-=======
     domains: ['images.unsplash.com', 'source.unsplash.com', 'firebasestorage.googleapis.com'],
->>>>>>> 6135e4561932e15df7ba19bad1a48493403680ee
   },
   reactStrictMode: true,
   i18n,
