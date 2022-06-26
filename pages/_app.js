732a49676f15dc785b08830435f6515af20482a3
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-06-26 22:44:30 +0530

    fix: displaing navbar and footer on every page

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,15 @@ import { appWithTranslation } from "next-i18next";
 
 import { store } from "../app/store";
 import { Provider } from "react-redux";
+import Navbar from "../components/navbar/Navbar"
+import Footer from "../components/footer/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </Provider>
   );
 }
