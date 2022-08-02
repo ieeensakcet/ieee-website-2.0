e8d7aa8c23f7bf0c112fe52100016d42d5a8bff1
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2022-08-02 14:39:30 +0530

    fix: diaplay card image

--- pages/events/[id]/index.js
+++ pages/events/[id]/index.js
@@ -29,7 +29,7 @@ export default function Events() {
     const unsub = onSnapshot(doc(db, "events", `${id}`), (doc) => {
       if (doc.exists()) {
         setdata(doc.data());
-        setImage(doc.data().images[1])
+        setImage(doc.data().images[0])
       } else {
         alert("Event not found");
       }
